384e629c89d9913d539c785334ef912dff0fc5a4
Mahdi (Arta) Hamidishandiz <99989899+Thesadsatan@users.noreply.github.com>
2023-10-22 12:23:26 +0300

    Update __init__.py

--- Final/app/__init__.py
+++ Final/app/__init__.py
@@ -27,8 +27,8 @@ login_manager.login_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('Quodophile_USERNAME')
-app.config['MAIL_PASSWORD'] = os.environ.get('Quodophile_SMTP_PASS')
+app.config['MAIL_USERNAME'] = 'noreply.quodophile@gmail.com'
+app.config['MAIL_PASSWORD'] = 'ttap smqo uzhp jqhb'
 
 mail = Mail(app)
 
